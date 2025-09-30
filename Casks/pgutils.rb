27877d4cf5ebf1227ef691558c4b1fd92c9f61dc
Jordan Layfield <77847327+layfield-ccdc@users.noreply.github.com>
2025-09-30 17:02:00 +0100

    Update pgutils.rb

--- Casks/pgutils.rb
+++ Casks/pgutils.rb
@@ -1,21 +1,18 @@
-cask Pgutil do
-  arch arm: "arm64"
+cask "pgutils" do
+  version "2.1.13.1"
+  sha256 "9a830eaf3cd079dedd262b25e13949281c910d5475ed267f0765cb15b5b0daf0"
+
+  url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-arm64.zip"
+  name "pgutil"
   desc "CLI for performing actions with ProGet"
   homepage "https://inedo.com"
-  url "https://github.com/Inedo/pgutil/releases/download/v2.1.13.1/pgutil-osx-arm64.zip"
-  sha256 "9a830eaf3cd079dedd262b25e13949281c910d5475ed267f0765cb15b5b0daf0"
-  license "MIT"
+
+  depends_on arch: :arm64
+
+  binary "pgutil"
 
   livecheck do
-    url :stable
-  end
-
-
-  def install
-    bin.install "pgutil"
-  end
-
-  test do
-    assert_match "Usage", shell_output("#{bin}/pgutil --help", 255)
+    url :url
+    strategy :github_latest
   end
 end
