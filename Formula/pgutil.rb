0efd39bc068dc6341a9e4a3dbd5e319b84a5f63e
Jordan Layfield <77847327+layfield-ccdc@users.noreply.github.com>
2025-09-30 13:29:24 +0100

    Update pgutil.rb

--- Formula/pgutil.rb
+++ Formula/pgutil.rb
@@ -1,8 +1,7 @@
 class Pgutil < Formula
     desc "CLI for performaing actinos with ProGet"
     homepage "https://inedo.com"
-    url "https://github.com/Inedo/pgutil/releases/download/v{version}/pgutil-osx-arm64.zip"
-    version "2.1.13.1"
+    url "https://github.com/Inedo/pgutil/releases/download/v2.1.13.1/pgutil-osx-arm64.zip"
     sha256 "9a830eaf3cd079dedd262b25e13949281c910d5475ed267f0765cb15b5b0daf0"
     license "MIT"
   
