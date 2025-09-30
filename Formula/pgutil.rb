16eab56fa86d2e704bd7ce7f9211579d6c84b017
Jordan Layfield <layfield@ccdc.cam.ac.uk>
2025-09-30 13:22:16 +0100

    pgutil NO_JIRA

---
+++ Formula/pgutil.rb
@@ -0,0 +1,18 @@
+class Pgutil < Formula
+    desc "CLI for performaing actinos with ProGet"
+    homepage "https://inedo.com"
+    url "https://github.com/Inedo/pgutil/releases/download/v#{version}/pgutil-osx-arm64.zip"
+    version "2.1.13"
+    sha256 "9a830eaf3cd079dedd262b25e13949281c910d5475ed267f0765cb15b5b0daf0"
+    license "MIT"
+  
+  
+    def install
+      bin.install "pgutil"
+    end
+  
+    test do
+      assert_match "Usage", shell_output("#{bin}/pgutil --help")
+    end
+  end
+  
